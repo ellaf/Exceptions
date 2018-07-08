3c25e0a800d46fdc7cd38639b0880f45d586d766
ellaf <league@iMac-2.attlocal.net>
2018-07-08 11:32:05 -0700

    Started exceptions! :)

--- src/_02_gotta_catchem_all/ExceptionsTest.java
+++ src/_02_gotta_catchem_all/ExceptionsTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 
-class ExceptionsTest {
+class hub {
 	ExceptionMethods em = new ExceptionMethods();
 	
 	//1. This is an example of how one might test that an exception is thrown.
